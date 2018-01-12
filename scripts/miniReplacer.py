f3f02916ec485f80524e1543ec7478f8c5d511be
Henning Kirschenmann <henning.kirschenmann@cern.ch>
2018-01-12 14:31:51 +0100

    update line to fix in L2L3Residual files for Summer16_07Aug2017_V4 (reco::FormulaEvaluator doesn't know TMath::Exp)

--- scripts/miniReplacer.py
+++ scripts/miniReplacer.py
@@ -4,15 +4,16 @@ import sys
 
 def replace_line(file_name, line_num, text):
     lines = open(file_name, 'r').readlines()
+    print("will replace\n",lines[line_num],"\n by\n",text)
     lines[line_num] = text
     out = open(file_name, 'w')
     out.writelines(lines)
     out.close()
 
-newLine= "{ 1 JetEta 1 JetPt [2]*([3]*([4]+[5]*TMath::Log(max([0],min([1],x))))*1./([6]+[7]*100./3.*(TMath::Max(0.,1.03091-0.051154*pow(x,-0.154227))-TMath::Max(0.,1.03091-0.051154*TMath::Power(208.,-0.154227)))+[8]*(-0.978049+(1+0.04432-1.304*pow(max(30.,min(6500.,x)),-0.4624)+(0+1.724*TMath::Log(max(30.,min(6500.,x))))/max(30.,min(6500.,x)))))) Correction L2Relative}\n"
+newLine= "{ 1 JetEta 1 JetPt [2]*([3]*([4]+[5]*TMath::Log(max([0],min([1],x))))*1./([6]+[7]*100./3.*(TMath::Max(0.,1.03091-0.051154*pow(x,-0.154227))-TMath::Max(0.,1.03091-0.051154*TMath::Power(208.,-0.154227)))+[8]*0.021*(-1.+1./(1.+exp(-(TMath::Log(x)-5.030)/0.395))))) Correction L2Relative}\n"
 
-print "will call replace_line(",sys.argv[1],",0,",newLine,")"
-#replace_line(sys.argv[1] , 0, newLine)
+print("will call replace_line(",sys.argv[1],",0,",newLine,")")
+replace_line(sys.argv[1] , 0, newLine)
 
 
 
