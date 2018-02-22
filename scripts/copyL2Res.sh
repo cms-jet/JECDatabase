062c183f598e6ed59a88c4b33f8598f77b86b3d5
karavdin <karavdina@gmail.com>
2018-02-22 17:30:43 +0100

    preliminary residuals for 2017 data

--- scripts/copyL2Res.sh
+++ scripts/copyL2Res.sh
@@ -21,7 +21,13 @@ TO=$2
 #Name_L2Res="Summer16_07Aug2017_pT_LOGLIN_L2Residual_pythia8_AK4PFchs.txt"
 
 #pt-balance with loglin for |eta|>1.3 and const for |eta|<1.3
-Name_L2Res="Summer16_07Aug2017_pT_Hybrid_Barrel_L2Residual_pythia8_AK4PFchs.txt"
+#Name_L2Res="Summer16_07Aug2017_pT_Hybrid_Barrel_L2Residual_pythia8_AK4PFchs.txt"
+
+#pt-balance with const for preliminary L2Res
+Name_L2Res="Fall17_17Nov2017_pT_FLAT_L2Residual_pythia8_AK4PFchs.txt"
+
+#MPF with const for preliminary L2Res
+#Name_L2Res="Fall17_17Nov2017_MPF_FLAT_L2Residual_pythia8_AK4PFchs.txt"
 
 
 L3Res_new=$3" "$4" "$5
