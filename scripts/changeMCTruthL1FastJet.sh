1f1143221949f67b9c6fea88882944c211814da1
karavdin <karavdina@gmail.com>
2018-06-21 18:17:38 +0200

    updated MCTruth for Fall17 MC (all jet collections)

--- scripts/changeMCTruthL1FastJet.sh
+++ scripts/changeMCTruthL1FastJet.sh
@@ -13,21 +13,20 @@ TO=$2
 for jet_coll in AK4PF AK4PFchs AK4PFPuppi AK8PF AK8PFchs AK8PFPuppi 
 do
 #How it's called in files from MCTruth analyser
-#Name_L2Rel="Fall17_25nsV2_MC_L2Relative_"$jet_coll".txt"
-    Name_L2Rel="Fall17_25nsV1_MC_L2Relative_"$jet_coll".txt"
+    Name_L1FastJet="Fall17_25nsV1_MC_L1FastJet_"$jet_coll".txt"
     
 #How it should be called
-    Name_L2Rel_official=${TO}_L2Relative_$jet_coll".txt"
+    Name_L1FastJet_official=${TO}_L1FastJet_$jet_coll".txt"
 
 #tar -zxvf ${FROM}.tar.gz
     f=${FROM}
     t=${TO}
 
     echo "Cloning $f to $t"
-    echo "File name $f/$Name_L2Rel"
+    echo "File name $f/$Name_L1FastJet"
 # #cp -r $f $t
-    cp $f/${Name_L2Rel} $t/${Name_L2Rel_official}
+    cp $f/${Name_L1FastJet} $t/${Name_L1FastJet_official}
 
-    echo "Check the file: "$t/${Name_L2Rel_official}
+    echo "Check the file: "$t/${Name_L1FastJet_official}
     echo "Done"
 done
