444bff6582399cc901df98e6026ccd005fcd714d
karavdin <karavdina@gmail.com>
2018-08-15 15:36:57 +0200

    Fixed L1RC for data

--- scripts/changeMCTruthL1FastJet.sh
+++ scripts/changeMCTruthL1FastJet.sh
@@ -11,10 +11,14 @@ TO=$2
 
 
 #for jet_coll in AK4PF AK4PFchs AK4PFPuppi AK8PF AK8PFchs AK8PFPuppi 
-for jet_coll in AK4PF AK4PFchs AK8PF AK8PFchs 
+#for jet_coll in AK4PF AK4PFchs AK8PF AK8PFchs 
+for jet_coll in AK4PF AK4PFchs
 do
 #How it's called in files from MCTruth analyser
-     Name_L1FastJet="Fall17_25nsV1_MC_L1FastJet_"$jet_coll".txt"
+#     Name_L1FastJet="Fall17_25nsV1_MC_L1FastJet_"$jet_coll".txt"
+     Name_L1FastJet="Fall17_17Nov2017_V22_MC_L1FastJet_"$jet_coll".txt"
+#    Name_L1FastJet="Fall17_17Nov2017B_V22_Data_L1FastJet_"$jet_coll".txt"
+
 #    Name_L1FastJet="Summer18_25nsV1_MC_L1FastJet_"$jet_coll".txt"
 #    Name_L1FastJet="bias2SelectionPow_25nsV1_MC_L1FastJet_"$jet_coll".txt"
     
